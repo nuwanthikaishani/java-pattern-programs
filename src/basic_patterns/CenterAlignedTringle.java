b8cd1816988d098ef8c5a96e3bf6e4fdce6099d8
Ishani Nuwanthika(QA Journal by Ish) <nuwanthikaishani@gmail.com>
2025-08-08 20:44:00 +0530

    updated - basic pattern design - tringle patterns

---
+++ src/basic_patterns/CenterAlignedTringle.java
@@ -0,0 +1,5 @@
+package basic_patterns;
+
+public class CenterAlignedTringle {
+
+}
---
+++ src/basic_patterns/LeftAlignedTriangle.java
@@ -0,0 +1,14 @@
+package basic_patterns;
+
+public class LeftAlignedTriangle {
+    public static void main(String args[]){
+        int n =5;
+
+        for(int i =1; i <= n - 1; i++ ){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
