b1c9ab831d8ef3e37e7f8accc0d3fb6dae27a1f9
Ishani Nuwanthika(QA Journal by Ish) <nuwanthikaishani@gmail.com>
2025-08-09 08:50:42 +0530

    updated - basic patterns - star and number patterns

--- src/basic_patterns/CenterAlignedTringle.java
+++ src/basic_patterns/CenterAlignedTringle.java
@@ -2,4 +2,24 @@ package basic_patterns;
 
 public class CenterAlignedTringle {
 
+    public static void main(String args[]){
+        int n =5;
+//        for(int i = 0; i <= n-1; i++){
+//            for(int j =0; j<i;j++){
+//                System.out.print(" * ");
+//            }
+//            System.out.println();
+//        }
+        for(int i =0; i<n; i++){
+            for(int j=n-i; j>1; j--){
+                System.out.print(" ");
+            }
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
 }
+
